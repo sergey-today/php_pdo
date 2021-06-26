ffb9f8866d7ce05829aed2bf60e622a522a93503
sergey-today <84334487+sergey-today@users.noreply.github.com>
2021-06-26 13:33:35 +0700

    Update base.inc.php

--- base.inc.php
+++ base.inc.php
@@ -1,28 +1,31 @@
 <?
 
 	class db {
+		
+		private
+			$host = 'host',
+			$dbname = 'dbname',
+			$user = 'user',
+			$password = 'password',
+			$charset = 'charset',
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
 
-		private $host = 'localhost';
-		private $dbname = 'default';
-		private $user = 'user';
-		private $password = 'password';
-		private $charset = 'utf8';
-		private $options = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
-
-		public function query($query, ...$q) {
-			$connect_str = "mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset";
-			$pdo = new PDO($connect_str, $this->user, $this->password, $this->options);
-			$DATA = $pdo->prepare($query);
-			$DATA->execute(array(...$q));
-			return $DATA;
+		function query($query, ...$q) {
+			$pdo = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset;",
+				$this->user,
+				$this->password,
+				$this->options
+			);
+			$data = $pdo->prepare($query);
+			$data->execute(array(...$q));
+			return $data;
 		}
 		
 	}
-
-	$db = new db();
 
 ?>
